72271e748b1eb1181f95a1a44dee3e4f0dac0bdb
tomaszkus97 <tomi88d@interia.pl>
2018-12-13 13:54:45 +0100

    last try

--- MyToolsYourToolsClient/src/app/offers-list/mockToolsTable.ts
+++ MyToolsYourToolsClient/src/app/offers-list/mockToolsTable.ts
@@ -3,7 +3,8 @@ import {Tool} from './tool';
 export const TOOLS: Tool[] = [
     {name:"Łopata", groupId:1,category:"kopiąceXD"},
     {name:"Kosiarka", groupId:2,category:"koszące"},
-    {name:"Piła mechaniczna", groupId:1,category:"piłujące"},
-    {name:"Nożyce do żywopłotu", groupId:3,category:"tnące"}
+    {name:"Piła mechaniczna", groupId:1,category:"piłujące"}
+    
+    
 
   ];
