a4500bcd668a8c934a452c5824cfaaeccf7fa47d
tomaszkus97 <tomi88d@gmail.com>
2018-12-13 13:49:43 +0100

    just to try... ;)

--- MyToolsYourToolsClient/src/app/offers-list/mockToolsTable.ts
+++ MyToolsYourToolsClient/src/app/offers-list/mockToolsTable.ts
@@ -3,6 +3,7 @@ import {Tool} from './tool';
 export const TOOLS: Tool[] = [
     {name:"Łopata", groupId:1,category:"kopiąceXD"},
     {name:"Kosiarka", groupId:2,category:"koszące"},
-    {name:"Piła mechaniczna", groupId:1,category:"piłujące"}
+    {name:"Piła mechaniczna", groupId:1,category:"piłujące"},
+    {name:"Nożyce do żywopłotu", groupId:3,category:"tnące"}
 
   ];
