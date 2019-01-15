c6d03d87134cfa39b0fb5982c960c955597ab93c
MAq2402 <maq0297@gmail.com>
2019-01-16 00:25:32 +0100

    Password validation

--- MyToolsYourToolsBackend/MyToolsYourToolsBackend.Application/Dtos/RegisterCredentialsDto.cs
+++ MyToolsYourToolsBackend/MyToolsYourToolsBackend.Application/Dtos/RegisterCredentialsDto.cs
@@ -9,6 +9,8 @@ namespace MyToolsYourToolsBackend.Application.Dtos
     {
         public string UserName { get; set; }
         public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
         public string ConfirmPassword { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
