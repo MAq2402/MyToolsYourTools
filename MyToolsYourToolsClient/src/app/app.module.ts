8a76db5203e470f16a33dec896707e86f2fb7f4b
Kuba <kubeckk21@gmail.com>
2019-01-19 21:38:59 +0100

    routed login page to root and added link to register page in login page

--- MyToolsYourToolsClient/src/app/app.module.ts
+++ MyToolsYourToolsClient/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { OfferService } from './services/offer.service';
 import { GroupService } from './services/group.service';
 import { UserService } from './services/user.service';
 import { RentService } from './services/rent.service';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
@@ -39,7 +40,8 @@ import { RentService } from './services/rent.service';
     OffersCreatorComponent,
     ToolDetailsComponent,
     RegisterComponent,
-    OfferViewComponent
+    OfferViewComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
