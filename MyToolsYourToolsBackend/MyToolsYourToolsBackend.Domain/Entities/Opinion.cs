e2e75b56f91ef997b5ba7200b560a7a39f91224a
Michal Miciak <maq0297@gmail.com>
2019-01-23 22:00:14 +0100

    Db updated

---
+++ MyToolsYourToolsBackend/MyToolsYourToolsBackend.Domain/Entities/Opinion.cs
@@ -0,0 +1,17 @@
+﻿using MyToolsYourToolsBackend.Domain.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyToolsYourToolsBackend.Domain.Entities
+{
+    public class Opinion: Enitity
+    {
+        public string Message { get; set; }
+        public Guid RatedUserId { get; set; }
+        public User RatedUser { get; set; }
+        public Guid RatingUserId { get; set; }
+        public User RatingUser { get; set; }
+
+    }
+}
--- MyToolsYourToolsBackend/MyToolsYourToolsBackend.Domain/Entities/User.cs
+++ MyToolsYourToolsBackend/MyToolsYourToolsBackend.Domain/Entities/User.cs
@@ -16,6 +16,10 @@ namespace MyToolsYourToolsBackend.Domain.Entities
         public IEnumerable<UserGroup> UserGroups { get; set; } = new List<UserGroup>();
         public IEnumerable<Offer> Offers { get; set; } = new List<Offer>();
         public IEnumerable<Rent> Rents { get; set; } = new List<Rent>();
+        public IEnumerable<Notification> TargetedNotifications { get; set; } = new List<Notification>();
+        public IEnumerable<Notification> SentNotifications { get; set; } = new List<Notification>();
+        public IEnumerable<Opinion> GivenOpinions { get; set; }
+        public IEnumerable<Opinion> ReceivedOpinions { get; set; }
         public int Points { get; set; }
     }
 }
