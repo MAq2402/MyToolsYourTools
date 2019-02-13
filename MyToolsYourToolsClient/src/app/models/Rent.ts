5229aa3a8bc299c4a550817a411a86e538f4359a
BartekBac <bac2@vp.pl>
2019-02-13 23:06:30 +0100

    added sending notifications after rent request and confirm return, adding/deleting rents ready

--- MyToolsYourToolsClient/src/app/models/Rent.ts
+++ MyToolsYourToolsClient/src/app/models/Rent.ts
@@ -2,4 +2,9 @@
 export class Rent {
     offerId: string;
     borrowerId: string;
+
+    constructor(offerId: string, borrowerId: string) {
+      this.offerId = offerId;
+      this.borrowerId = borrowerId;
+    }
   }
--- MyToolsYourToolsClient/src/app/services/rent.service.ts
+++ MyToolsYourToolsClient/src/app/services/rent.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
 import { Rent } from '../models/Rent';
+import { AlertService } from './alert.service';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -12,14 +14,14 @@ const httpOptions = {
 export class RentService {
   baseUrl = 'https://localhost:44341/api/';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private alertService: AlertService) { }
 
   addRent (rent: Rent): Observable<Rent> {
-    return this.http.post<Rent>(this.baseUrl + 'rents', rent, httpOptions);
+    return this.http.post<Rent>(this.baseUrl + 'rents', rent, httpOptions)
   }
 
-  deleteRent (offerId: string): Observable<any> {
-    return this.http.delete(this.baseUrl + 'rents/' + offerId, httpOptions);
+  deleteRent (offerId: string): Observable<Rent> {
+    return this.http.delete<Rent>(this.baseUrl + 'rents/' + offerId, httpOptions);
   }
 
 
